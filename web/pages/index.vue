911f6ab3b6dcbae535c9a089c08f703aeaf673de
AMIRI <mohamed69200>
2025-01-16 17:11:57 +0100

    fix: button of login and register

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,19 +1,27 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="12" md="6" lg="4">
-          <v-card>
-            <v-card-title class="text-h5">Bienvenue sur A'rosa-je</v-card-title>
-            <v-card-text>
-              <v-btn @click="goToLogin" color="success" block>Se connecter</v-btn>
-              <v-spacer></v-spacer>
-              <v-btn @click="goToRegister" color="secondary" block>S'inscrire</v-btn>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="6" lg="4">
+        <v-card>
+          <v-card-title class="text-h5 text-center">Bienvenue sur A'rosa-je</v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col>
+                <v-btn @click="goToLogin" color="success" block variant="flat">Se connecter</v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-btn @click="goToRegister" color="secondary" block variant="flat">S'inscrire</v-btn>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
   
   <script setup>
   const goToLogin = () => {
